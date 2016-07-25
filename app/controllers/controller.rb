acd61e753fa0b5e461625f08768c44f8fd0d7f1f
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-07-25 18:10:46 -0500

    First commit

---
+++ app/controllers/controller.rb
@@ -0,0 +1,22 @@
+
+MyApp.get "/" do
+	erb :"/"
+end
+
+
+# MyApp.get "/converter" do
+# 	erb :"/converter" 
+# end
+
+
+
+# # Create the converted value.
+# MyApp.post "" do
+# erb:"/"
+# end
+
+
+# # Get the value being converted.
+# MyApp.post "" do
+# erb:"/"
+# end
---
+++ app/models/models.rb
@@ -0,0 +1,83 @@
+
+# class Temperature
+# 	def initialize(celcius,fahrenheit)
+# 		@celcius = celcius
+# 		@fahrenheit = fahrenheit
+# 	end
+# 	def Fahrenheit_conv()
+# 		(@celcius*1.8)+32
+# 	end
+
+
+# 	def Celcius_conv()
+# 		(@fahrenheit-32)*(0.5556)
+# 	end
+# end
+
+
+
+# class Distance
+# 	def initialize(miles,kilometer)
+# 		@miles = miles
+# 		@kilometer = kilometer
+# 	end
+
+# 	def Kilometer_conv()
+# 		@miles * 1.609344
+# 	end
+
+# 	def Miles_conv()
+# 		@kilometer / 1.609344
+# 	end
+# end
+
+
+
+# class Weight
+# 	def initialize(pound,stone,kilogram)
+# 		@pound = pound
+# 		@stone = stone
+# 		@kilogram = kilogram
+# 	end
+
+# 	def get_pounds
+# 		@pound
+# 	end
+
+# 	def get_stones
+# 		@stone
+# 	end
+
+# 	def get_kilograms
+# 		@kilogram
+# 	end
+
+
+# 	def stoneToPounds()
+# 		@stone * 14
+# 	end
+
+# 	def stoneToKilogram()
+# 		@stone * 6.35029318
+# 	end
+
+# 	def kilogramToPounds()
+# 		@kilogram / 0.45359237
+# 	end
+
+# 	def kilogramToStone()
+# 		@kilogram / 6.35029318
+# 	end
+
+# 	def poundsToStone()
+# 		@pound / 14
+# 	end
+
+# 	def poundsToKilogram()
+# 		@pound * 0.45359237
+# 	end
+# end
+
+
+
+
